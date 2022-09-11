efbed1e6dd6e205188160ec1998dd94555ed64a3
Jeremy Moya <50006478+jeremy3500@users.noreply.github.com>
2022-09-11 17:32:14 -0500

    Add files via upload

---
+++ Game_Michi/Game_Michi/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Game_Michi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_05 = new System.Windows.Forms.Button();
+            this.btn_01 = new System.Windows.Forms.Button();
+            this.btn_02 = new System.Windows.Forms.Button();
+            this.btn_03 = new System.Windows.Forms.Button();
+            this.btn_04 = new System.Windows.Forms.Button();
+            this.btn_06 = new System.Windows.Forms.Button();
+            this.btn_08 = new System.Windows.Forms.Button();
+            this.btn_07 = new System.Windows.Forms.Button();
+            this.btn_09 = new System.Windows.Forms.Button();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_05
+            // 
+            this.btn_05.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_05.FlatAppearance.BorderSize = 0;
+            this.btn_05.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_05.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_05.Location = new System.Drawing.Point(157, 163);
+            this.btn_05.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_05.Name = "btn_05";
+            this.btn_05.Size = new System.Drawing.Size(70, 70);
+            this.btn_05.TabIndex = 1;
+            this.btn_05.Text = "5";
+            this.btn_05.UseVisualStyleBackColor = false;
+            this.btn_05.Click += new System.EventHandler(this.btn_05_Click);
+            // 
+            // btn_01
+            // 
+            this.btn_01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_01.FlatAppearance.BorderSize = 0;
+            this.btn_01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_01.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_01.ForeColor = System.Drawing.Color.Black;
+            this.btn_01.Location = new System.Drawing.Point(74, 80);
+            this.btn_01.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.btn_01.Name = "btn_01";
+            this.btn_01.Size = new System.Drawing.Size(70, 70);
+            this.btn_01.TabIndex = 1;
+            this.btn_01.Text = "1";
+            this.btn_01.UseVisualStyleBackColor = false;
+            this.btn_01.Click += new System.EventHandler(this.btn_01_Click);
+            // 
+            // btn_02
+            // 
+            this.btn_02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_02.FlatAppearance.BorderSize = 0;
+            this.btn_02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_02.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_02.Location = new System.Drawing.Point(157, 80);
+            this.btn_02.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.btn_02.Name = "btn_02";
+            this.btn_02.Size = new System.Drawing.Size(70, 70);
+            this.btn_02.TabIndex = 1;
+            this.btn_02.Text = "2";
+            this.btn_02.UseVisualStyleBackColor = false;
+            this.btn_02.Click += new System.EventHandler(this.btn_02_Click);
+            // 
+            // btn_03
+            // 
+            this.btn_03.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_03.FlatAppearance.BorderSize = 0;
+            this.btn_03.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_03.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_03.Location = new System.Drawing.Point(240, 80);
+            this.btn_03.Name = "btn_03";
+            this.btn_03.Size = new System.Drawing.Size(70, 70);
+            this.btn_03.TabIndex = 1;
+            this.btn_03.Text = "3";
+            this.btn_03.UseVisualStyleBackColor = false;
+            this.btn_03.Click += new System.EventHandler(this.btn_03_Click);
+            // 
+            // btn_04
+            // 
+            this.btn_04.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_04.FlatAppearance.BorderSize = 0;
+            this.btn_04.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_04.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_04.Location = new System.Drawing.Point(74, 163);
+            this.btn_04.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_04.Name = "btn_04";
+            this.btn_04.Size = new System.Drawing.Size(70, 70);
+            this.btn_04.TabIndex = 1;
+            this.btn_04.Text = "4";
+            this.btn_04.UseVisualStyleBackColor = false;
+            this.btn_04.Click += new System.EventHandler(this.btn_04_Click);
+            // 
+            // btn_06
+            // 
+            this.btn_06.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_06.FlatAppearance.BorderSize = 0;
+            this.btn_06.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_06.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_06.Location = new System.Drawing.Point(240, 163);
+            this.btn_06.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_06.Name = "btn_06";
+            this.btn_06.Size = new System.Drawing.Size(70, 70);
+            this.btn_06.TabIndex = 1;
+            this.btn_06.Text = "6";
+            this.btn_06.UseVisualStyleBackColor = false;
+            this.btn_06.Click += new System.EventHandler(this.btn_06_Click);
+            // 
+            // btn_08
+            // 
+            this.btn_08.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_08.FlatAppearance.BorderSize = 0;
+            this.btn_08.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_08.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_08.Location = new System.Drawing.Point(157, 246);
+            this.btn_08.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_08.Name = "btn_08";
+            this.btn_08.Size = new System.Drawing.Size(70, 70);
+            this.btn_08.TabIndex = 1;
+            this.btn_08.Text = "8";
+            this.btn_08.UseVisualStyleBackColor = false;
+            this.btn_08.Click += new System.EventHandler(this.btn_08_Click);
+            // 
+            // btn_07
+            // 
+            this.btn_07.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_07.FlatAppearance.BorderSize = 0;
+            this.btn_07.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_07.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_07.Location = new System.Drawing.Point(74, 246);
+            this.btn_07.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_07.Name = "btn_07";
+            this.btn_07.Size = new System.Drawing.Size(70, 70);
+            this.btn_07.TabIndex = 1;
+            this.btn_07.Text = "7";
+            this.btn_07.UseVisualStyleBackColor = false;
+            this.btn_07.Click += new System.EventHandler(this.btn_07_Click);
+            // 
+            // btn_09
+            // 
+            this.btn_09.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_09.FlatAppearance.BorderSize = 0;
+            this.btn_09.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_09.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_09.Location = new System.Drawing.Point(240, 246);
+            this.btn_09.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+            this.btn_09.Name = "btn_09";
+            this.btn_09.Size = new System.Drawing.Size(70, 70);
+            this.btn_09.TabIndex = 1;
+            this.btn_09.Text = "9";
+            this.btn_09.UseVisualStyleBackColor = false;
+            this.btn_09.Click += new System.EventHandler(this.btn_09_Click);
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
+            this.lblTurn.Location = new System.Drawing.Point(104, 24);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(106, 25);
+            this.lblTurn.TabIndex = 2;
+            this.lblTurn.Text = "Turno de:";
+            this.lblTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
+            this.lblName.Location = new System.Drawing.Point(207, 24);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(74, 25);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "NAME";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(384, 353);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.btn_09);
+            this.Controls.Add(this.btn_06);
+            this.Controls.Add(this.btn_03);
+            this.Controls.Add(this.btn_02);
+            this.Controls.Add(this.btn_07);
+            this.Controls.Add(this.btn_04);
+            this.Controls.Add(this.btn_08);
+            this.Controls.Add(this.btn_01);
+            this.Controls.Add(this.btn_05);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_05;
+        private System.Windows.Forms.Button btn_01;
+        private System.Windows.Forms.Button btn_02;
+        private System.Windows.Forms.Button btn_03;
+        private System.Windows.Forms.Button btn_04;
+        private System.Windows.Forms.Button btn_06;
+        private System.Windows.Forms.Button btn_08;
+        private System.Windows.Forms.Button btn_07;
+        private System.Windows.Forms.Button btn_09;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ Game_Michi/Game_Michi/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Game_Michi
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { InitializeComponent(); }
+        string turn = ""; 
+        ArrayList lista = new ArrayList();
+        bool[] jug_play = new bool[9]; bool game = true, victory;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_01.Text = null; btn_02.Text = null; btn_03.Text = null; btn_04.Text = null;
+            btn_05.Text = null; btn_06.Text = null; btn_07.Text = null; btn_08.Text = null; btn_09.Text = null;
+
+            turn = "player";
+            lblName.Text = turn;
+            
+        }
+        public void verify_turn(Button btn, int num)
+        {
+            if (turn == "player")
+            {
+                turn = "bot"; btn.BackColor = Color.FromArgb(47, 74, 157); btn.Enabled = false; // COLOR azul
+                jug_play[num - 1] = true; verify_victory();
+            }
+            else
+            {
+                turn = "player"; btn.BackColor = Color.FromArgb(145, 31, 39); btn.Enabled = false; // COLOR red 
+            }
+            if (game) { lblName.Text = turn; wait(1000); }
+        }
+        public void turno_bot()
+        {
+            if (game)
+            {
+                if (turn == "bot" && lista.Count < 9)
+                {
+                    bool repet = false; int n;
+                    do
+                    {
+                        repet = false;
+                        Random rnd = new Random();
+                        n = rnd.Next(1, 10);
+                        foreach (int i in lista)
+                        {
+                            if (i == n) { repet = true; break; }
+                            Thread.Sleep(100);
+                        }
+                    } while (repet);
+                    switch (n)
+                    {
+                        case 1: btn_01.PerformClick(); break;
+                        case 2: btn_02.PerformClick(); break;
+                        case 3: btn_03.PerformClick(); break;
+                        case 4: btn_04.PerformClick(); break;
+                        case 5: btn_05.PerformClick(); break;
+                        case 6: btn_06.PerformClick(); break;
+                        case 7: btn_07.PerformClick(); break;
+                        case 8: btn_08.PerformClick(); break;
+                        case 9: btn_09.PerformClick(); break;
+                    }
+                }
+            }
+        }
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer"); 
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer"); 
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+        public void verify_victory()
+        {
+            if (jug_play[0] && jug_play[1] && jug_play[2]) victory = true;
+            if (jug_play[3] && jug_play[4] && jug_play[5]) victory = true;
+            if (jug_play[6] && jug_play[7] && jug_play[8]) victory = true;
+
+            if (jug_play[0] && jug_play[3] && jug_play[6]) victory = true;
+            if (jug_play[1] && jug_play[4] && jug_play[7]) victory = true;
+            if (jug_play[2] && jug_play[5] && jug_play[8]) victory = true;
+
+            if (jug_play[0] && jug_play[4] && jug_play[8]) victory = true;
+            if (jug_play[2] && jug_play[4] && jug_play[6]) victory = true;
+
+            if (victory) { game = false; MessageBox.Show("\tGanaste\t\t"); }
+        }
+        private void btn_01_Click(object sender, EventArgs e) { lista.Add(1); verify_turn(btn_01, 1); turno_bot(); }
+        private void btn_02_Click(object sender, EventArgs e) { lista.Add(2); verify_turn(btn_02, 2); turno_bot(); }
+        private void btn_03_Click(object sender, EventArgs e) { lista.Add(3); verify_turn(btn_03, 3); turno_bot(); }
+        private void btn_04_Click(object sender, EventArgs e) { lista.Add(4); verify_turn(btn_04, 4); turno_bot(); }
+        private void btn_05_Click(object sender, EventArgs e) { lista.Add(5); verify_turn(btn_05, 5); turno_bot(); }
+        private void btn_06_Click(object sender, EventArgs e) { lista.Add(6); verify_turn(btn_06, 6); turno_bot(); }
+        private void btn_07_Click(object sender, EventArgs e) { lista.Add(7); verify_turn(btn_07, 7); turno_bot(); }
+        private void btn_08_Click(object sender, EventArgs e) { lista.Add(8); verify_turn(btn_08, 8); turno_bot(); }
+        private void btn_09_Click(object sender, EventArgs e) { lista.Add(9); verify_turn(btn_09, 9); turno_bot(); }
+    }
+}
